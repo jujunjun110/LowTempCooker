7ceaf823c906a9de8d7a27b797dc0d6f0cb959d1
Jun Ito <jun_ito@voyagegroup.com>
2017-02-22 16:39:47 +0900

    Add: first commit

---
+++ LowTempCooker.ino
@@ -0,0 +1,55 @@
+int relayPin = 13;
+int thermoPin = 0;
+int loopDuration = 1000;
+float temp_low = 67.0; // 温度の下限
+float temp_high = 69.0; // 温度の上限
+
+long pastMilliSecs = 0;
+int pastMinutes = 0;
+
+void setup(){
+  Serial.begin(9600);
+  Serial.println("Cooking Start!");
+  pinMode(relayPin, OUTPUT);
+}
+
+void loop(){
+  float temperature = getTemperature();
+  Serial.println(temperature);
+  
+  turnPotIfNeeded(temperature);
+  printTimeIfNeeded();
+
+  pastMilliSecs += loopDuration;
+  delay(loopDuration); 
+}
+
+float getTemperature () {
+  float analogInput; // 0〜203
+  float temp_c = 0; // 摂氏( ℃ )
+  analogInput = analogRead(thermoPin);
+  temp_c = (500 * analogInput) / 1024; // 入力値を摂氏に換算
+  return temp_c;  
+}
+
+void turnPotIfNeeded (float temp) {
+  if (temp < 0.0 || temp > 100.0) {
+    Serial.println("invalid temp. data omitted");
+    digitalWrite(relayPin, LOW);
+    return;
+  }  
+  if (temp < temp_low) {
+    digitalWrite(relayPin, HIGH);
+  }
+  if (temp > temp_high) {
+    digitalWrite(relayPin, LOW);
+  }   
+}
+
+void printTimeIfNeeded() {
+  int newPastMinutes = pastMilliSecs / 60000;
+  if (newPastMinutes > pastMinutes) {
+    pastMinutes = newPastMinutes;
+    Serial.println("\n" + String(pastMinutes) + " minutes have passed.\n");
+  } 
+}
